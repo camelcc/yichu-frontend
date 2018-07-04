a001f5218dead119c908eb422437be817d9f14b3
camel <camel.young@gmail.com>
2018-07-03 22:25:26 -0700

    basic ui page

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,62 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      <p> 粘贴 Youtube 链接： </p>
+      <p><input v-model="videoUrl"></p>
+      <button v-on:click="parseUrl()">获取视频</button>
+    </div>
+    <PlayList v-if="parsed" v-bind:video="videoData"/>
+    <p v-if="error">{{ error }}</p>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
+import PlayList from './components/PlayList.vue'
 
 export default {
-  name: 'app',
+  data() {
+    return {
+      videoUrl: "",
+
+      videoData: {},
+
+      parsed: false,
+      error: "",
+    }
+  },
+  methods: {
+    parseUrl() {
+      // clear status
+      this.error = ""
+      this.parsed = false
+
+
+      var encodedUrl = encodeURI(this.videoUrl)
+      axios.get('/parse', {
+        params: {
+          page: encodedUrl
+        }
+      })
+      .then((response) => {
+        // eslint-disable-next-line
+        console.log(response.data)
+        this.videoData = response.data
+        this.parsed = true
+      })
+      .catch((error) => {
+        this.error = error
+      })
+    }
+  },
   components: {
-    HelloWorld
+    PlayList
   }
 }
+
 </script>
 
-<style lang="scss">
+<style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/PlayList.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <p v-if="video.type != 'video'">目前不支持视频列表解析</p>
+        <div v-if="video.type == 'video'">
+            <p>{{ video.description }}</p>
+            <video controls v-bind:poster="video.thumbnail" v-bind:src="video.url" width="720px"></video>
+            <a :href="'yichu://video/' + urlScheme() ">Open App</a>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'PlayList',
+  props: {
+    video: Object
+  },
+  methods: {
+      urlScheme() {
+          return btoa(unescape(encodeURIComponent(JSON.stringify(this.video))));
+      }
+  }
+}
+</script>
+
+<style scoped>
+</style>
